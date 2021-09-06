8f10bdb42ef8acf36aa8eafb67b15b2ae0705fe5
ViktorKiselev98 <77990767+ViktorKiselev98@users.noreply.github.com>
2021-09-06 18:55:03 +0300

    fasf
    fsaf

--- Source/Tankogeddon/Tankogeddon.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Tankogeddon.h"
-#include "Modules/ModuleManager.h"
-
-//DEFINE_LOG_CATEGORY(LogTankogeddon);
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tankogeddon, "Tankogeddon" );
